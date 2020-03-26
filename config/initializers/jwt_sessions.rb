c0cf147a25cc457d8d0b5f80462f47030eb441b6
vimiomori <vivian.muchen@gmail.com>
2020-03-26 14:44:29 +0900

    use memory instead of redis for stores for now

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -1,1 +1,8 @@
+# JWTSessions.token_store = :redis, {
+#   redis_host: "127.0.0.1",
+#   redis_port: "6379",
+#   redis_db_name: "0",
+#   token_prefix: "jwt_"
+# }
+JWTSessions.token_store = :memory
 JWTSessions.encryption_key = 'secret'
